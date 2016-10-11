ec22a434f51d1ff09b63dc1ed645edb37e98c19c
Matthias Wettstein <matthias.wettstein@zoho.com>
2016-10-11 15:24:18 +0200

    Play around with PG, add text to story

--- pg.py
+++ pg.py
@@ -56,16 +56,16 @@ def policy_forward(x):
 def policy_backward(eph, epdlogp):
 	""" backward pass. (eph is array of intermediate hidden states) """
 	dW2 = np.dot(eph.T, epdlogp).ravel()
-	dh = np.outer(epdlogp, model['W2'])
-	dh[eph <= 0] = 0 # backpro prelu
+	dh = np.outer(epdlogp, model['W2']) ### layer_1_loss = np.dot(layer_2_wloss, W2.T)
+	dh[eph <= 0] = 0 # backpro prelu ### layer_1_wloss = layer_1_loss * sigmoid_derivative(layer_1)
 	dW1 = np.dot(dh.T, epx)
 	return {'W1':dW1, 'W2':dW2}
 
 env = gym.make("Pong-v0")
 observation = env.reset()
 prev_x = None # used in computing the difference frame
-xs,hs,dlogps,drs = [],[],[],[]
-running_reward = None
+xs,hs,dlogps,drs = [],[],[],[] ### FOR FULL BACKPROP ONCE DONE
+running_reward = None ### DISCOUNTED?
 reward_sum = 0
 episode_number = 0
 while True:
@@ -78,14 +78,15 @@ while True:
 
 	# forward the policy network and sample an action from the returned probability
 	aprob, h = policy_forward(x)
-	action = 2 if np.random.uniform() < aprob else 3 # roll the dice!
+	### 
+	action = 2 if np.random.uniform() < aprob else 3 # roll the dice!   
+	
 
 	# record various intermediates (needed later for backprop)
 	xs.append(x) # observation
 	hs.append(h) # hidden state
 	y = 1 if action == 2 else 0 # a "fake label"
 	dlogps.append(y - aprob) # grad that encourages the action that was taken to be taken (see http://cs231n.github.io/neural-networks-2/#losses if confused)
-	print y, aprob, y - aprob
 
 	# step the environment and get new measurements
 	observation, reward, done, info = env.step(action)
@@ -97,27 +98,28 @@ while True:
 		episode_number += 1
 
 		# stack together all inputs, hidden states, action gradients, and rewards for this episode
-		epx = np.vstack(xs)
-		eph = np.vstack(hs)
-		epdlogp = np.vstack(dlogps)
-		epr = np.vstack(drs)
+		epx = np.vstack(xs) ### STACK ALL INPUTS
+		eph = np.vstack(hs) ### STACK ALL HIDDEN LAYERS: WHAT FOR?
+		epdlogp = np.vstack(dlogps) ### STACK ALL LOSSES
+		epr = np.vstack(drs) ### STACK ALL REWARDS
 		xs,hs,dlogps,drs = [],[],[],[] # reset array memory
 
 		# compute the discounted reward backwards through time
-		discounted_epr = discount_rewards(epr)
+		discounted_epr = discount_rewards(epr) # DISCOUNT REWARDS EX POST
 		# standardize the rewards to be unit normal (helps control the gradient estimator variance)
-		discounted_epr -= np.mean(discounted_epr)
+		discounted_epr -= np.mean(discounted_epr) ### ONLY POSSIBLE SINCE ALL REWARDS ARE KNOWN
 		discounted_epr /= np.std(discounted_epr)
 
-		epdlogp *= discounted_epr # modulate the gradient with advantage (PG magic happens right here.)
-		grad = policy_backward(eph, epdlogp)
-		for k in model: grad_buffer[k] += grad[k] # accumulate grad over batch
+		epdlogp *= discounted_epr # modulate the gradient with advantage (PG magic happens right here.) ### ELEMENT-WISE MULTIPLY LOSSES WITH DISCOUNTED REWARDS <-> SGD, WEIGHT LOSSES
+		grad = policy_backward(eph, epdlogp) ### STORED HIDDEN LAYERS, STORED WEIGHTED LOSSES
+		for k in model: # FOR EACH LAYER K
+			grad_buffer[k] += grad[k] # accumulate grad over batch ###???
 
 		# perform rmsprop parameter update every batch_size episodes
 		if episode_number % batch_size == 0:
 			for k,v in model.iteritems():
-				g = grad_buffer[k] # gradient
-				rmsprop_cache[k] = decay_rate * rmsprop_cache[k] + (1 - decay_rate) * g**2
+				g = grad_buffer[k] # gradient ###???
+				rmsprop_cache[k] = decay_rate * rmsprop_cache[k] + (1 - decay_rate) * g**2 ###???
 				model[k] += learning_rate * g / (np.sqrt(rmsprop_cache[k]) + 1e-5)
 				grad_buffer[k] = np.zeros_like(v) # reset batch gradient buffer
 
